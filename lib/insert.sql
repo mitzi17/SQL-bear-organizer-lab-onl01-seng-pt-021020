89d2b2f2482a7d6215ff2d5a29ca70a787becf36
Mitzi Estrada <m_estrada17@live.com>
2020-04-12 08:13:21 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr.Chocolate", 20, "m", "dark brown","calm", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 10, "m", "black", "intense", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 6, "f", "dark brown", "nice", 0);
+INSERT INTO bears VALUES (4,"Sergeant Brown", 19, "m", "green", "slimy", 1);
+INSERT INTO bears VALUES (5,"Melissa", 13, "f", "dark brown", "goofy", 0);
+INSERT INTO bears VALUES (6,"Grinch", 2, "m", "black", "grinchy", 1);
+INSERT INTO bears VALUES (7,"Wendy", 6, "f", "blue", "naive", 0);
+INSERT INTO bears VALUES (8,NULL, 20, "m", "black", "aggressive", 0);
